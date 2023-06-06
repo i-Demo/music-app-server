9d90f4557930af99e39e48a49d8e88570f0e9489
Hoai Vi <hoaivi.it@gmail.com>
2023-06-06 12:51:25 +0700

    Update Package.json

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const db = require("./configs/db");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 5000;
 const route = require("./routes");
 // const bodyParser = require("body-parser");
 // Connect database
